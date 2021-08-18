1f77c167721eafa3dead4b8592c163009958064c
Carlos Priego <carlos.priego@ibm.com>
2021-08-18 10:02:14 -0500

    feat: show jest output and archive results

--- template/dojo-helper.sh
+++ template/dojo-helper.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-node_modules/.bin/jest --coverage --verbose --notify --json --outputFile=test-results.json || true
+node_modules/.bin/jest --colors --coverage --verbose --notify --json --outputFile=test-results.json >| test-results.colored.txt 2>&1
 commit_msg="$(node dojo-helper.js)"
+cat test-results.colored.txt
+cat test-results.colored.txt | sed -e $'s/\x1b\[[0-9;]*m//g' >| test-results.txt
 rm test-results.json
+rm test-results.colored.txt
 # echo "GIT_COMMIT_MSG > $commit_msg"
 git add -A
 git commit -m "$commit_msg" || true
